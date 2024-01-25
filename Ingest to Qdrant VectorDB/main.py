6a474c276bf2fd683baa3661548d19b5e91c7a48
Merlin Carter <merlin+demo@quix.io>
2024-01-25 14:03:36 +0000

    Edited 'main.py'

--- Ingest to Qdrant VectorDB/main.py
+++ Ingest to Qdrant VectorDB/main.py
@@ -39,7 +39,7 @@ app = Application.Quix(
 )
 
 # Define an input topic with JSON deserializer
-input_topic = app.topic(os.environ['input'], value_deserializer="json")
+input_topic = app.topic(os.environ['input'], value_deserializer="quix")
 
 # Initialize a streaming dataframe based on the stream of messages from the input topic:
 sdf = app.dataframe(topic=input_topic)
