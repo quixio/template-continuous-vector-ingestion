bf511514138de90568d200ca0c86a29b45580b9f
Steve <100689438+SteveRosam@users.noreply.github.com>
2024-01-29 11:58:25 +0000

    Merge current pipeline to DEV (#1)

---
+++ Create Embeddings/main.py
@@ -0,0 +1,45 @@
+from quixstreams import Application
+from sentence_transformers import SentenceTransformer
+import os
+import time
+
+encoder = SentenceTransformer('all-MiniLM-L6-v2') # Model to create embeddings
+
+# Define the embedding function
+def create_embeddings(row):
+
+    # link,headline,category,short_description,authors,date
+
+    text = row['short_description'] #  Merlin.. i changed this to match the incoming data
+    embeddings = encoder.encode(text)
+    embedding_list = embeddings.tolist() # Conversion step because SentenceTransformer outputs a numpy Array but Qdrant expects a plain list
+    print(f'Created vector: "{embedding_list}"')
+
+    return embedding_list
+
+app = Application.Quix(
+    "vectorizer",
+    auto_offset_reset="earliest",
+    auto_create_topics=True,  # Quix app has an option to auto create topics
+)
+
+# Define an input topic with JSON deserializer
+input_topic = app.topic(os.environ['input'], value_deserializer="json") #  Merlin! I changed these from "quix" to "json"
+
+# Define an output topic with JSON serializer
+output_topic = app.topic(os.environ['output'], value_serializer="json")
+
+# Initialize a streaming dataframe based on the stream of messages from the input topic:
+sdf = app.dataframe(topic=input_topic)
+sdf = sdf.update(lambda val: print(f"Received update: {val}"))
+
+# Trigger the embedding function for any new messages(rows) detected in the filtered SDF
+sdf["embeddings"] = sdf.apply(create_embeddings, stateful=False)
+
+# Update the timestamp column to the current time in nanoseconds
+sdf["Timestamp"] = sdf["Timestamp"].apply(lambda row: time.time_ns())
+
+# Publish the processed SDF to a Kafka topic specified by the output_topic object.
+sdf = sdf.to_topic(output_topic)
+
+app.run(sdf)
---
+++ Ingest to Qdrant Cloud VectorDB/main.py
@@ -0,0 +1,54 @@
+from quixstreams import Application
+from sentence_transformers import SentenceTransformer
+from qdrant_client import models, QdrantClient
+import os
+
+qdrant = QdrantClient(
+    url="https://620342be-1e5e-401c-98da-42bcaddaed57.us-east4-0.gcp.cloud.qdrant.io:6333", 
+    api_key=os.environ['qdrant_apikey'],
+)
+
+encoder = SentenceTransformer('all-MiniLM-L6-v2') # Model to create embeddings
+collection = os.environ['collectionname']
+
+# Create collection to store items
+qdrant.recreate_collection(
+    collection_name=collection,
+    vectors_config=models.VectorParams(
+        size=encoder.get_sentence_embedding_dimension(), # Vector size is defined by used model
+        distance=models.Distance.COSINE
+    )
+)
+
+# Define the ingestion function
+def ingest_vectors(row):
+
+  single_record = models.PointStruct(
+    id=row['index'],
+    vector=row['embeddings'],
+    payload=row
+    )
+
+  qdrant.upload_points(
+      collection_name=collection,
+      points=[single_record]
+    )
+
+  print(f'Ingested vector entry id: "{row["index"]}"...')
+
+app = Application.Quix(
+    "vectorizercloud",
+    auto_offset_reset="earliest",
+    auto_create_topics=True,  # Quix app has an option to auto create topics
+)
+
+# Define an input topic with JSON deserializer
+input_topic = app.topic(os.environ['input'], value_deserializer="json") # Merlin.. i updated this for you
+
+# Initialize a streaming dataframe based on the stream of messages from the input topic:
+sdf = app.dataframe(topic=input_topic)
+
+# INGESTION HAPPENS HERE
+### Trigger the embedding function for any new messages(rows) detected in the filtered SDF
+sdf = sdf.update(lambda row: ingest_vectors(row))
+app.run(sdf)
